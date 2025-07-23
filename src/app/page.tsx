e2645e81321755e09c562e6468ea8d321503dfaf
raminetemadi1997 <raminrasi7@gmail.com>
2025-07-23 10:10:01 +0330

    switched to npm from pnpm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import BannerContainer from "@/components/ui/Banner/BannerContainer";
-import Carousel from "@/components/ui/Carousel";
-import HomeContent from "@/components/ui/Home/HomeContent";
+
 
 const Home = () => {
   return (
